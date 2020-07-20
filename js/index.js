cb280ea140b13331c57f526b4406e35e6f353938
Przemyslaw_Filewicz <filcu97@gmail.com>
2020-07-20 17:41:37 +0200

    Dodano komentarze do poszczególnych funkcji w pliku js, zmieniono wygląd tła.

--- js/index.js
+++ js/index.js
@@ -1,54 +1,12 @@
-<<<<<<< HEAD
+//function calculating the BMI pointer
 
-    const solve = () => { 
-        let weight = document.querySelector(".value-weight input").value;
-        let height = document.querySelector(".value-height input").value;
-        let result = weight/(0.01*height)**2;
-        let rounded_result = result.toFixed(2);
-    
-        if (weight>30 && height>120 && weight <300 && height < 250) {
-            document.getElementById("results").innerHTML = ("Twoje BMI wynosi - " + rounded_result);
-            check(rounded_result);
-        } else { 
-            document.getElementById("results").innerHTML="Podano nieprawidłowe dane!";
-            document.querySelector(".interpretation").innerHTML = "Podaj właściwe dane";
-        };
-    }
-
-
-const check = (rounded_result) => {
-    if (rounded_result < 18.5) {
-        document.querySelector(".interpretation").innerHTML = "Wskazuje to na niedowagę. Konieczne jest dożywienie organizmu. Zalecana jest konsultacja z dietetykiem celem zwiększenia masy ciała."
-    } else if (18.5 < rounded_result && rounded_result < 25) { 
-        document.querySelector(".interpretation").innerHTML = "Wynik oznacza prawidłową masę ciała. Utrzymanie wskaźnika o takiej wartości gwarantuje atracyjny wygląd i zdrowie."
-    } else if (25 <= rounded_result && rounded_result < 30) { 
-        document.querySelector(".interpretation").innerHTML = "Wynik klasyfikowany jest jako nadwaga. Nie jest to powód do zmiartwień jednak zalecana jest nieznaczna redukcja masy ciała."
-    } else if (30 <= rounded_result && rounded_result < 35) { 
-        document.querySelector(".interpretation").innerHTML = "Oznacza to otyłość I stopnia. Zalecana jest redukcja masy ciała i wskazane są konsultacje z dietetykiem. Utrzymywanie się takiego stanu dwukrotnie zwiększa ryzyko zachorowania na choroby cywilizacyjne."
-    } else if (35 <= rounded_result && rounded_result < 40) { 
-        document.querySelector(".interpretation").innerHTML = "Wskazuje to na otyłość II stopnia. Należy zgłosić się do lekarza specjalisty celem ustalenia planu redukcji masy ciała. Taki stan poważnie zagraża zdrowiu!"
-    } else if (rounded_result>40){
-        document.querySelector(".interpretation").innerHTML = "Oznacza to otyłość III stopnia. Taki stan jest uznawany za zagrażający życiu! Zaleca się pilną konsultację z lekarzem specjalistą, aby pod jego kontrolą rozpocząć odchudzanie."
-    } else {
-        document.querySelector(".interpretation").innerHTML = "Podaj właściwe dane"
-    };
-}
-
-const ent = (e) => {
-    let e1 = e.keyCode;
-    if (e1==13) {
-        solve();
-    }
-};
-document.addEventListener("keypress", ent);
-=======
 function solve() {
 	let weight = document.querySelector(".value-weight input").value;
 	let height = document.querySelector(".value-height input").value;
 	let result = weight / (0.01 * height) ** 2;
 	let rounded_result = result.toFixed(2);
 
-	if (weight > 30 && height > 120 && weight < 300 && height < 250) {
+	if (weight > 30 && height > 120 && weight < 300 && height < 290) {  //validation of entered data
 		document.getElementById("results").innerHTML =
 			"Twoje BMI wynosi - " + rounded_result;
 		check(rounded_result);
@@ -57,6 +15,8 @@ function solve() {
 		document.querySelector(".interpretation").innerHTML = "Podaj właściwe dane";
 	}
 }
+
+//results interpretation
 
 function check(rounded_result) {
 	if (rounded_result < 18.5) {
@@ -71,7 +31,7 @@ function check(rounded_result) {
 	} else if (30 <= rounded_result && rounded_result < 35) {
 		document.querySelector(".interpretation").innerHTML =
 			"Oznacza to otyłość I stopnia. Zalecana jest redukcja masy ciała i wskazane są konsultacje z dietetykiem. Utrzymywanie się takiego stanu dwukrotnie zwiększa ryzyko zachorowania na choroby cywilizacyjne.";
-	} else if (35 <= rounded_result && rounded_result < 40) {
+	} else if (35 <= rounded_result && rounded_result <= 40) {
 		document.querySelector(".interpretation").innerHTML =
 			"Wskazuje to na otyłość II stopnia. Należy zgłosić się do lekarza specjalisty celem ustalenia planu redukcji masy ciała. Taki stan poważnie zagraża zdrowiu!";
 	} else if (rounded_result > 40) {
@@ -81,11 +41,12 @@ function check(rounded_result) {
 		document.querySelector(".interpretation").innerHTML = "Podaj właściwe dane";
 	}
 }
->>>>>>> feature/filerek
+
+//submitting by Enter
 
 const ent = (e) => {
 	var el = e.keyCode;
-	if (el == 13) {
+	if (el == 13) {				             	
 		solve();
 	}
 };
